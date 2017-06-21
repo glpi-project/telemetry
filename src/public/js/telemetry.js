b3ae81d845fa560e84684a7d588f6bd597c721ea
Alexandre Delaunay <delaunay.alexandre@gmail.com>
2017-06-21 17:41:17 +0200

    add some animations

--- src/public/js/telemetry.js
+++ src/public/js/telemetry.js
@@ -1,37 +1,114 @@
 $(document).ready(function() {
 
-   new Chartist.Pie('#php_versions',
+   var php_versions = new Chartist.Pie('#php_versions',
                     $('#php_versions').data("id"),
                     {
+                        donut: true,
                         plugins: [
                            Chartist.plugins.tooltip()
                         ]
                     });
+   animateDonut(php_versions);
 
-   new Chartist.Pie('#glpi_versions',
+   var glpi_versions = new Chartist.Pie('#glpi_versions',
                     $('#glpi_versions').data("id"),
                     {
+                        donut: true,
                         plugins: [
                            Chartist.plugins.tooltip()
                         ]
                     });
+   animateDonut(glpi_versions);
 
-   new Chartist.Bar('#top_plugins',
+   var top_plugins = new Chartist.Bar('#top_plugins',
                     $('#top_plugins').data("id"),
                     {
                         // horizontalBars: true,
                         distributeSeries: true,
+                        axisX: {
+                           showGrid: false,
+                        },
+                        axisY: {
+                           showGrid: false,
+                           showLabel: false,
+                        },
                         plugins: [
                            Chartist.plugins.tooltip()
                         ]
                     });
+   animateSimpleBar(top_plugins);
 
-   new Chartist.Pie('#os_family',
+   var os_family = new Chartist.Pie('#os_family',
                     $('#os_family').data("id"),
                     {
+                        donut: true,
                         plugins: [
                            Chartist.plugins.tooltip()
                         ]
                     });
+   animateDonut(os_family);
 
 });
+
+
+var animateDonut = function (chart) {
+   chart.on('draw', function(data) {
+      if(data.type === 'slice') {
+         // Get the total path length in order to use for dash array animation
+         var pathLength = data.element._node.getTotalLength();
+
+         // Set a dasharray that matches the path length as prerequisite to animate dashoffset
+         data.element.attr({
+            'stroke-dasharray': pathLength + 'px ' + pathLength + 'px'
+         });
+
+         // Create animation definition while also assigning an ID to the animation for later sync usage
+         var animationDefinition = {
+            'stroke-dashoffset': {
+               id: 'anim' + data.index,
+               dur: 500,
+               from: -pathLength + 'px',
+               to:  '0px',
+               easing: Chartist.Svg.Easing.easeOutQuint,
+               // We need to use `fill: 'freeze'` otherwise our animation will fall back to initial (not visible)
+               fill: 'freeze'
+            }
+         };
+
+         // If this was not the first slice, we need to time the animation so that it uses the end sync event of the previous animation
+         if(data.index !== 0) {
+            animationDefinition['stroke-dashoffset'].begin = 'anim' + (data.index - 1) + '.end';
+         }
+
+         // We need to set an initial value before the animation starts as we are not in guided mode which would do that for us
+         data.element.attr({
+            'stroke-dashoffset': -pathLength + 'px'
+         });
+
+         // We can't use guided mode as the animations need to rely on setting begin manually
+         // See http://gionkunz.github.io/chartist-js/api-documentation.html#chartistsvg-function-animate
+         data.element.animate(animationDefinition, false);
+     }
+   });
+};
+
+var animateSimpleBar = function(chart) {
+   chart.on('draw', function(data) {
+      if(data.type === 'bar') {
+         data.element.animate({
+            y2: {
+            dur: 1000,
+            from: data.y1,
+            to: data.y2,
+            easing: Chartist.Svg.Easing.easeOutQuint
+         },
+         opacity: {
+            dur: 1000,
+            from: 0,
+            to: 1,
+            easing: Chartist.Svg.Easing.easeOutQuint
+         }
+       });
+     }
+   });
+}
