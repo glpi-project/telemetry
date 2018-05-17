dd541b0995147e0da8270827c711a4fca7656838
Aymeric Lafontaine <alafontaine@teclib.com>
2018-05-17 14:05:52 +0200

    Admin view

---
+++ app/Controllers/Admin.php
@@ -0,0 +1,255 @@
+<?php namespace GLPI\Telemetry\Controllers;
+
+use GLPI\Telemetry\Controllers\ControllerAbstract;
+use GLPI\Telemetry\Models\Reference as ReferenceModel;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class Admin extends ControllerAbstract
+{
+    public function view(Request $req, Response $res, array $args)
+    {
+        $get = $req->getQueryParams();
+
+        /**
+         * if status is not specified in parameter $args :
+         * default -> pending
+         * and if the status is specified in the $_SESSION, we'll take it.
+         * That allow the pagination doesn't specified any status parameter.
+		**/
+        if(!isset($args['status'] )){
+        	$args['status'] = 1;
+        	if($_SESSION['reference']['status_page_admin'] !== null){
+        		$args['status'] = $_SESSION['reference']['status_page_admin'];
+        	}
+        }
+
+        // default session param for this controller
+        if (!isset($_SESSION['reference'])) {
+            $_SESSION['reference'] = [
+                "orderby" => 'created_at',
+                "sort"    => "desc"
+            ];
+        }
+
+        // manage sorting
+        if (isset($get['orderby'])) {
+            if ($_SESSION['reference']['orderby'] == $get['orderby']) {
+               // toggle sort if orderby requested on the same column
+                $_SESSION['reference']['sort'] = ($_SESSION['reference']['sort'] == "desc"
+                                                ? "asc"
+                                                : "desc");
+            }
+            $_SESSION['reference']['orderby'] = $get['orderby'];
+        }
+        $_SESSION['reference']['pagination'] = 15;
+        $_SESSION['reference']['status_page_admin'] = $args['status'];
+
+        //check for refences presence
+        $dyn_refs = $this->container->project->getDynamicReferences();
+        if (false === $dyn_refs) {
+             // retrieve data from model
+            $references = ReferenceModel::active()->orderBy(
+                $_SESSION['reference']['orderby'],
+                $_SESSION['reference']['sort']
+            )->paginate($_SESSION['reference']['pagination']);
+        } else {
+            try {
+                $join_table = $this->container->project->getSlug() . '_reference';
+                $order_field = $_SESSION['reference']['orderby'];
+                $order_table = (isset($dyn_refs[$order_field]) ? $join_table : 'reference');
+                // retrieve data from model
+                $ref = new ReferenceModel();
+                $model = $ref->newInstance();
+                $model = call_user_func_array(
+                    [
+                        $model,
+                        'select'
+                    ],
+                    array_merge(
+                        ['reference.*'],
+                        array_map(
+                            function ($key) use ($join_table) {
+                                return $join_table . '.' . $key;
+                            },
+                            array_keys($dyn_refs)
+                        )
+                    )
+                );
+                $model->where('status', '=', $args['status']);
+                $model->orderBy(
+                    $order_table . '.' . $order_field,
+                    $_SESSION['reference']['sort']
+                )
+                    ->leftJoin($join_table, 'reference.id', '=', $join_table . '.reference_id')
+                ;
+                $references = $model->paginate($_SESSION['reference']['pagination']);
+                //var_dump($_SESSION['reference']);die;
+            } catch (\Illuminate\Database\QueryException $e) {
+                if ($e->getCode() == '42P01') {
+                    //relation does not exists
+                    throw new \RuntimeException(
+                        'You have configured dynamic references for your project; but table ' .
+                        $join_table . ' is missing!',
+                        0,
+                        $e
+                    );
+                }
+                throw $e;
+            }
+        }
+
+        $ref_ref = new ReferenceModel;
+        $ref = $ref_ref->newInstance();
+        foreach ($references as $reference) {
+        	$mails = $ref->findMails($reference['attributes']['id']);
+        	if($mails !== false){
+        		$ref_user_mails[$reference['attributes']['id']] = $mails;
+        	}else{
+        		$ref_user_mails[$reference['attributes']['id']] = null;
+        	}
+        }
+
+
+
+        $references->setPath($this->container->router->pathFor('admin'));
+
+        // render in twig view
+        $this->render($this->container->project->pathFor('admin.html.twig'), [
+            'class'         => 'admin',
+            'showmodal'     => isset($get['showmodal']),
+            'uuid'          => isset($get['uuid']) ? $get['uuid'] : '',
+            'references'    => $references,
+            'pagination'    => $references->appends($_GET)->render(),
+            'orderby'       => $_SESSION['reference']['orderby'],
+            'sort'          => $_SESSION['reference']['sort'],
+            'dyn_refs'      => $dyn_refs,
+            'user_session'	=> $_SESSION['user'],
+            'status_page'	=> $args['status'],
+            'ref_user_mails'=> json_encode($ref_user_mails)	
+        ]);
+    }
+
+    public function filter(Request $req, Response $res, array $args)
+    {
+        $get = $req->getQueryParams();
+
+        // manage sorting
+        if (isset($args['orderby'])) {
+            if ($_SESSION['reference']['orderby'] == $args['orderby']) {
+               // toggle sort if orderby requested on the same column
+                $_SESSION['reference']['sort'] = ($_SESSION['reference']['sort'] == "desc"
+                                                ? "asc"
+                                                : "desc");
+            }
+            $_SESSION['reference']['orderby'] = $args['orderby'];
+        }
+
+        return $res->withRedirect($this->container->router->pathFor('admin'));
+    }
+
+    public function ActionReferencePost(Request $req, Response $res)
+    {
+    	$post = $req->getParsedBody();
+    	$tmp = [];
+    	if(isset($post['checkboxAdminActionForm1'])){
+    		$tmp[] = $post['checkboxAdminActionForm1'];
+    	}
+    	if(isset($post['checkboxAdminActionForm2'])){
+    		$tmp[] = $post['checkboxAdminActionForm2'];
+    	}
+    	if(isset($post['inputrow3col2'])){
+    		$tmp[] = $post['inputrow3col2'];
+    	}
+
+    	return $this->ActionReference($req, $res,
+    		[
+    			'ref_id' => $post['ref_id_input'],
+    			'status' => $post['status_input'],
+    			'comment' => $post['comment'],
+    			'mails'	=> $tmp
+    		]
+    	);
+    }
+
+    public function ActionReference(Request $req, Response $res, array $args)
+    {
+        $get = $req->getQueryParams();
+        $ref_ref = new ReferenceModel;
+        $ref = $ref_ref->newInstance();
+
+        $ref_status_before_update_res = $ref->where("id", "=", $args['ref_id'])->firstOrFail();
+        $ref_status_before_update = $ref_status_before_update_res['attributes']['status'];
+
+        $res_update = $ref->updateStatus($args['ref_id'], $args['status']);
+
+        if($res_update == 1){
+        	$type = "success";
+        	$msg_text = "Action done";
+        	foreach ($args['mails'] as $key => $mail_to) {
+        		$this->sendMail($req, $res, $args, $ref_status_before_update, $mail_to);
+        	}
+        }else{
+        	$type = "error";
+        	$msg_text = "An error happened, bad insert, $res_update rows were updated, 1 row to update was expected";
+        }
+
+        $this->container->flash->addMessage(
+            $type,
+            $msg_text
+        );
+
+        return $res->withRedirect($this->container->router->pathFor('sorterAdmin', ['status'=>$ref_status_before_update]));
+    }
+
+    public function sendMail(Request $req, Response $res, array $args, $status_from, $mail_to)
+    {
+
+        $ref = new ReferenceModel();
+        $ref_model = $ref->newInstance();
+
+        $status_from = $ref_model->statusIntToText($status_from);
+        $status_to = $ref_model->statusIntToText($args['status']);
+
+        $join_table = $this->container->project->getSlug() . '_reference';
+
+        $res_ref = $ref_model->where("reference.id", "=", $args['ref_id'])        
+        ->leftJoin($join_table, $join_table.'.reference_id', '=', 'reference.id')
+        ->firstOrFail();
+
+        if(isset($args['comment']) && !empty($args['comment'])){
+        	$admin_msg = "Administrator's message :\n".$args['comment'];
+        }else{
+        	$admin_msg = "";
+        }
+
+        $msg_ref = 
+        "
+        Name : " . $res_ref['attributes']['name'] ."\n
+        Country : " . $res_ref['attributes']['country'] ."\n
+        Assets : " . $res_ref['attributes']['num_assets'] ."\n
+        Helpdesk : " . $res_ref['attributes']['num_helpdesk'] ."\n
+        Registration date : " . date("d M Y, h:i a",strtotime($res_ref['attributes']['created_at'])) ."\n
+        Comment : " . $res_ref['attributes']['comment']."\n";
+
+        $msg = "The status of your reference below has been changed by the admin from $status_from to $status_to :\n$msg_ref\n$admin_msg";
+
+        // prepare mail
+        $mail = new \PHPMailer;
+        $mail->setFrom($this->container['settings']['mail_admin']);
+        $mail->addAddress($mail_to);
+        $mail->Subject = "A new message from telemetry site : Reference status changed";
+        $mail->Body    = $msg;
+        $send_ok = $mail->send();
+
+        if($send_ok === false){
+	        $this->container->flash->addMessage(
+	            "error",
+	            "Error sending the mail to $mail_to.\n".$mail->ErrorInfo
+	        );
+        	return false;
+        } else {
+        	return true;
+        }
+    }
+}
---
+++ public/js/admin_action.js
@@ -0,0 +1,114 @@
+function actionInfoToForm(ref_id, status, mails){
+  $('#ref_id_input_id').val(ref_id);
+  $('#status_input_id').val(status);
+  $('#table_mails_action').prepend(createTable(mails[ref_id]));
+}
+
+
+ function renderSubmitButtonAdminActionForm(){
+ 	ck1 = document.getElementById('checkboxAdminActionForm1') ;
+	ck2 = document.getElementById('checkboxAdminActionForm2');
+	ck3 = document.getElementById('checkboxAdminActionForm3');
+
+	if (ck1!=null && ck1.checked || ck2!=null && ck2.checked || ck3!=null && ck3.checked) {
+		document.getElementById('submitAdminActionForm').disabled = false;
+	}else{
+		document.getElementById('submitAdminActionForm').disabled = true;
+	}
+
+	if(ck3!=null && ck3.checked){
+		document.getElementById('inputrow3col2id').disabled = false;
+	}else{
+		document.getElementById('inputrow3col2id').disabled = true;
+	}
+ }
+
+function createTable(mails_obj) {
+
+    // Create table.
+    var el = document.getElementById('tableFormActionAdmin');
+    if(el != null){
+    	el.remove();
+    }
+    var table = document.createElement('table');
+    table.setAttribute('id', 'tableFormActionAdmin')
+    table.className = "table table-striped glpi_references";
+
+    if(mails_obj['user_mail']){
+	    // Insert New Row for table at index '0'.
+	    var row1 = table.insertRow();
+	    // Insert New Column for Row1 at index '0'.
+	    var row1col1 = row1.insertCell(0);
+	    row1col1.innerHTML = 'User mail';
+	    // Insert New Column for Row1 at index '1'.
+	    var row1col2 = row1.insertCell(1);
+	    row1col2.innerHTML = mails_obj['user_mail'];
+
+	   	var row1col3 = row1.insertCell(2);
+	    checkbox1 = document.createElement('input');
+	    checkbox1.setAttribute('type', 'checkbox');
+	    checkbox1.setAttribute('onchange', 'renderSubmitButtonAdminActionForm()');
+	    checkbox1.setAttribute('id', 'checkboxAdminActionForm1');
+	    checkbox1.setAttribute('name', 'checkboxAdminActionForm1');
+	    checkbox1.setAttribute('value', mails_obj['user_mail'])
+	    row1col3.prepend(checkbox1);
+    }
+
+    if(mails_obj['ref_mail']){
+	    // Insert New Row for table at index '1'.
+	    var row2 = table.insertRow();
+	    // Insert New Column for Row2 at index '0'.
+	    var row2col1 = row2.insertCell(0);
+	    row2col1.innerHTML = 'Reference mail';
+	    // Insert New Column for Row2 at index '1'.
+	    var row2col2 = row2.insertCell(1);
+	    row2col2.innerHTML = mails_obj['ref_mail'];
+
+	    var row2col3 = row2.insertCell(2);
+	    checkbox2 = document.createElement('input');
+	    checkbox2.setAttribute('type', 'checkbox');
+	   	checkbox2.setAttribute('onchange', 'renderSubmitButtonAdminActionForm()');
+	    checkbox2.setAttribute('id', 'checkboxAdminActionForm2');
+	    checkbox2.setAttribute('name', 'checkboxAdminActionForm2');
+	    checkbox2.setAttribute('value', mails_obj['ref_mail'])
+	    row2col3.prepend(checkbox2);
+    }
+
+	// Insert New Row for table at index '2'.
+    var row3 = table.insertRow();
+    // Insert New Column for Row3 at index '0'.
+    var row3col1 = row3.insertCell(0);
+    row3col1.innerHTML = 'Or send to';
+    // Insert New Column for Row3 at index '1'.
+    var row3col2 = row3.insertCell(1);
+    var inputrow3col2 = document.createElement('input');
+    inputrow3col2.setAttribute('type', 'email');
+    inputrow3col2.setAttribute('id', 'inputrow3col2id');
+    inputrow3col2.setAttribute('required', true);
+    inputrow3col2.setAttribute('name', 'inputrow3col2');
+    inputrow3col2.disabled = true;
+    row3col2.prepend(inputrow3col2);
+
+	var row3col3 = row3.insertCell(2);
+    checkbox3 = document.createElement('input');
+    checkbox3.setAttribute('type', 'checkbox');
+   	checkbox3.setAttribute('onchange', 'renderSubmitButtonAdminActionForm()');
+    checkbox3.setAttribute('id', 'checkboxAdminActionForm3');
+    checkbox3.setAttribute('name', 'checkboxAdminActionForm3');
+    row3col3.prepend(checkbox3);
+
+    return table;
+}
+
+
+
+function validateAdminActionForm(){
+	ck1 = document.getElementById('checkboxAdminActionForm1');
+	ck2 = document.getElementById('checkboxAdminActionForm2');
+	ck3 = document.getElementById('checkboxAdminActionForm3');
+
+	if (ck1!=null && !ck1.checked && ck2!=null && !ck2.checked && ck3!=null && !ck3.checked) {
+		return false;
+	}
+	return true;
+}
